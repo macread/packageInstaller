dcede52893a4894225b5fe4a16ce6c8fb29c5db4
James 'Mac' Read <james.macgregor.read@gmail.com>
2018-08-16 14:43:41 -0600

    Premilinary input validation

--- src/packageInstaller.js
+++ src/packageInstaller.js
@@ -1,6 +1,12 @@
 module.exports = {
 
     packageInstaller: function(pkgs){
+        // validate input
+
+        if (typeof pkgs !== 'array') {
+            return 'err: an array is required input'
+        }
+
         let installOrder = ''
 
         return installOrder; 
--- src/packageInstaller.test.js
+++ src/packageInstaller.test.js
@@ -6,8 +6,20 @@ describe('packageInstaller Response Properties:', () => {
         expect( typeof pkg.packageInstaller ).toBe('function');
     })
     
-    test('packageInstaller returns a string', () => {
+    test('returns a string', () => {
         expect( typeof pkg.packageInstaller('[ "KittenService: CamelCaser", "CamelCaser: " ]') ).toBe('string')
+    })
+
+})
+
+describe('packageInstaller input validations:', () => {
+
+    test('Not an array error (empty string)', () => {
+        expect( pkg.packageInstaller('') ).toBe('err: an array is required input')
+    })
+
+    test('Not an array error (empty object)', () => {
+        expect( pkg.packageInstaller({}) ).toBe('err: an array is required input')
     })
 
 })
